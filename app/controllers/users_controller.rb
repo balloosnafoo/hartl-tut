2ef46d66b5f29d9f06caed6c9a50a3a839a859b4
Matthew Skinner <skinnerm@email.arizona.edu>
2015-03-09 18:15:55 +0100

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -18,7 +19,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) 
     if @user.save
-      @user.send_activation_email
+      # I seem to have skipped a step somewhere... this line or next needed
+      # to be deleted.
+      # @user.send_activation_email
       UserMailer.account_activation(@user).deliver_now
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
@@ -52,14 +55,6 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                     :password_confirmation)
-    end
-
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
     end
 
     def correct_user
